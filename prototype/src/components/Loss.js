31db7f3683a82f7653ab6a54e3deaf6463186fc1
Shelby Smitherman <smithermansl@gmail.com>
2018-08-07 16:40:46 -0400

    dynamic dot moves on click

--- prototype/src/components/Loss.js
+++ prototype/src/components/Loss.js
@@ -8,20 +8,42 @@ export default class Loss extends Component {
         x: 39,
         y: 42
       },
-      dynamicDot: {}
+      dynamicDot: {
+        x: -15,
+        y: -15
+      }
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (evt) {
+    evt.preventDefault()
+    // set dynamicDot x & y based on evt target location
+    this.setState({
+      dynamicDot: {
+        x: evt.nativeEvent.clientX,
+        y: evt.nativeEvent.clientY
+      }
+    })
   }
 
   render () {
-    const style = {
+    console.log('state after', this.state.dynamicDot)
+    const staticStyle = {
       position: 'absolute',
       left: this.state.staticDot.x,
       top: this.state.staticDot.y
+    },
+    dynamicStyle = {
+      position: 'absolute',
+      left: this.state.dynamicDot.x,
+      top: this.state.dynamicDot.y
     }
 
     return (
-      <div id="field">
-        <div id="staticDot" style={style}></div>
+      <div id="field" onClick={this.handleClick}>
+        <div id="staticDot" style={staticStyle}></div>
+        <div id="dynamicDot" style={dynamicStyle}></div>
       </div>
     )
   }
