42063c19a9e80bcd0ea7cf05a801ecbdb451bc53
Ali Ebner <ebner.ali@gmail.com>
2018-08-09 17:30:44 -0400

    models and oauth

---
+++ server/db/models/mazeData.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const MazeData = db.define('maze_data', {
+	x: {
+		type: Sequelize.INTEGER
+		allowNull: false
+	},
+	y: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	value: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	}
+})
+
+module.exports = MazeData
