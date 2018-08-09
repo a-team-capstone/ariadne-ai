1a28f877612c35751754cd87d8c980c578cd265b
Ali Ebner <ebner.ali@gmail.com>
2018-08-09 17:55:34 -0400

    ignored secrets

--- secrets.js
+++ secrets.js
@@ -1,3 +1,3 @@
 process.env.GOOGLE_CLIENT_ID = '420364437336-ct7drqkc811l56ou08pfaue2k3t44gui.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'chCFhD6BhIedMSKgCzr2_rug'
+process.env.GOOGLE_CLIENT_SECRET = 'jlcE9f2kgxIgEln5WcoVxiH6'
 process.env.GOOGLE_CALLBACK = '/auth/google/callback'
