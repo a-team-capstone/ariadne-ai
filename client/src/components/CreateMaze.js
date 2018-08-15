7c49b00644d79477bdecf186aadadf4922fd2d11
Alexandra Havia <sasha.havia@gmail.com>
2018-08-15 11:17:49 -0400

    After upload user automatically taken to flood-fill page

--- client/src/components/CreateMaze.js
+++ client/src/components/CreateMaze.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import UploadImage from './UploadImage'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
-const CreateMaze = ({ image }) => {
+const CreateMaze = () => {
 	return (
 		<div>
 			<h3>Get Creative</h3>
 			<div>
-				{Object.keys(image).length ? (
-					<Link to="/flood-fill">
-						<button type="button" className="btn btn-info">
-							Check if it is solvable
-						</button>
-					</Link>
-				) : (
-					<UploadImage />
-				)}
+				<UploadImage />
 			</div>
 		</div>
 	)
 }
 
-const mapState = state => {
-	return {
-		image: state.image
-	}
-}
-
-export default connect(
-	mapState,
-	null
-)(CreateMaze)
+export default CreateMaze
