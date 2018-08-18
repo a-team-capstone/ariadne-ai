755f26730b40b208b4afb2532a8ec03b99f450c1
Alexandra Havia <sasha.havia@gmail.com>
2018-08-17 20:03:44 -0400

    Updated Home component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { auth } from '../store/user'
-import { WiredButton, WiredInput } from 'wired-elements'
+import 'wired-elements'
 
 /**
  * COMPONENT
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-// import { Login } from './Form'
 import { Link } from 'react-router-dom'
-import { WiredButton } from 'wired-elements'
+import 'wired-elements'
 
 class Home extends Component {
 	render() {
@@ -9,15 +8,9 @@ class Home extends Component {
 			<div className="home">
 				<h2>ariadne.ai</h2>
 				<Link to="/login">
-					{/* <button type="button" className="btn btn-primary">
-						Login
-					</button> */}
 					<wired-button id="login-btn">Login</wired-button>
 				</Link>
 				<Link to="/sign-up">
-					{/* <button type="button" className="btn btn-primary">
-						Sign up
-					</button> */}
 					<wired-button id="signup-btn">Sign Up</wired-button>
 				</Link>
 			</div>
