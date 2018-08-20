37cff3cc7f1ff79feefe2fbb42c2b3a85bc8d1e4
Shelby Smitherman <smithermansl@gmail.com>
2018-08-20 12:44:26 -0400

    reseed

--- client/src/utilities/tutorialContent.js
+++ client/src/utilities/tutorialContent.js
@@ -4,48 +4,80 @@ const tutorialContent = [
     text: 'Draw a maze with power-ups and play it virtually, right away!'
   },
   {
-    header: 'Maze Dimensions',
-    text: 'You can use straight or curvy lines, just try to make your maze roughly 4 x 3 proportionately!',
-    image: 'image/of/exampleDimensions'
+    header: 'Maze Format',
+    subhead: 'Drawing should be 4 x 3 proportionately.',
+    text: 'Use straight or curvy lines, or build a maze from objects around you!',
+    image: ''
   },
   {
     header: 'Basic Gameplay',
-    text: 'The object of any maze is to navigate from START to END in the shortest amount of time! You may draw an integer within your maze photo to set the number of seconds a player has to solve. Without it, the default will be 30 seconds!',
-    image: 'image/start/end/withTime'
+    subhead: 'Navigate from start to finish.',
+    text: 'You can use the built-in navigation panel, or arrow keys if on a computer.',
+    image: ''
+  },
+  {
+    header: 'Timer',
+    subhead: 'Beat your maze in the shortest time possible!',
+    text:  'Draw an integer within your maze photo and the timer will count down from that number. Default time is 30 seconds.',
+    image: ''
   },
   {
     header: 'Power-Ups',
-    text: 'There are certain power-ups you can draw in your maze to make them more fun and challenging to play. Learn about them on the next few pages.'
+    subhead: 'Power-ups increase maze complexity, and fun!',
+    text: 'Draw them within your maze boundaries. Use each power-up only once per maze.',
+    image: ''
   },
   {
-    header: 'ST',
-    text: 'ST means start! Draw ST in your maze wherever you would like your character to start the game. If you do not draw ST, your character will automatically start at the top-left corner.',
-    image: 'image/of/STpoint'
+    header: 'STA',
+    subhead: 'STA is start!',
+    text: 'Draw STA in your maze wherever you would like your character to start in the maze. Default is the top-left corner.',
+    image: ''
   },
   {
     header: 'END',
-    text: 'END is the end. Navigate to wherever you draw END to solve the maze! If you do not draw END, you can beat your maze by navigating to the bottom-right corner.',
-    image: 'image/of/ENDpoint'
+    subhead: 'END is the end!',
+    text: 'Navigate to wherever you draw END to solve the maze! Default ending point is the bottom-right corner.',
+    image: ''
   },
   {
-    header: 'XT',
-    text: 'XT means extra time! Draw an XT in your maze and give players the option to navigate to it for more time on the clock, in case they are running out!',
-    image: 'image/of/XT'
+    header: 'XTM',
+    subhead: 'XTM is extra time!',
+    text: 'Navigate to an XT to add 10 seconds to the timer (if you need more time to solve)!',
+    image: ''
   },
   {
-    header: 'BM',
-    text: 'BM stands for bomb!! Draw BM in your maze to add an extra tricky challenge. If a player hits a BM, they lose automatically!',
-    image: 'image/of/BM'
+    header: 'BMB',
+    subhead: 'BMB is bomb!',
+    text: 'Draw BMB in your maze to add an additional challenge. If a player hits a bomb, they are sent back to start.',
+    image: ''
   },
   {
-    header: 'FZ',
-    text: 'FZ stands for freeze... brrrr! Whenever a player hits a freeze, they will be paused in place for 10 seconds... but the timer will still be counting down!',
-    image: 'image/of/FZ'
+    header: 'FRZ',
+    subhead: 'FRZ is freeze!',
+    text: 'Whenever a player hits a freeze, they will be paused in place for 5 seconds... but the timer will still be counting down!',
+    image: ''
   },
   {
     header: 'TEL / PRT',
-    text: 'These two go hand in hand... they stand for teleport! Transport your character back and forth across your maze quickly by hitting TEL or PRT respectively. They must both be present in a maze to function.',
-    image: 'image/of/TELPRT'
+    subhead: 'TEL and PRT together make teleport!',
+    text: 'Transport your character back and forth across the maze by hitting TL or PT respectively. They must both be present in a maze to function correctly!',
+    image: ''
+  },
+  {
+    header: 'Solvability',
+    subhead: 'Our algorithm will let you know if your maze is solvable.',
+    text: `Keep in mind, your power-ups may make it solvable (or not) depending on where you place them, so this isn't the end-all, be-all of solvability.`,
+    image: ''
+  },
+  {
+    header: 'Save and Send',
+    text: 'If you like the maze you created, save it, then send the maze to your friends to challenge them!',
+    image: ''
+  },
+  {
+    header: `Now You're Ready`,
+    text: 'Start drawing or building your first maze now!',
+    image: ''
   }
 ]
 
