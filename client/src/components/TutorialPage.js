6eb2a9a43a18467733981040bbb3c1825129b7ca
Alexandra Havia <sasha.havia@gmail.com>
2018-08-21 15:25:03 -0400

    Feature (mazes): Random name is generated when you save maze to db

--- client/src/components/TutorialPage.js
+++ client/src/components/TutorialPage.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import tutorialContent from '../utilities/tutorialContent'
 import { Link } from 'react-router-dom'
 
--- server/db/models/maze.js
+++ server/db/models/maze.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 const makeMatrix = require('../../utilities/makeMatrix')
+const generateName = require('../../utilities/randomNames')
 
 const Maze = db.define('maze', {
-  // name: {
-  //   type: Sequelize.STRING,
-  //   unique: true
-  // },
+	name: {
+		type: Sequelize.STRING
+	},
 	image: {
 		type: Sequelize.STRING,
 		unique: true
@@ -15,44 +15,53 @@ const Maze = db.define('maze', {
 		type: Sequelize.BOOLEAN
 	},
 	data: {
-    type: Sequelize.TEXT,
-    get () {
-      return makeMatrix(this.getDataValue('data'))
-    },
-    set (matrix) {
-      this.setDataValue('data', matrix.reduce((acc, curr) => {
-        acc = acc.concat(curr)
-        return acc
-      }, []).join(''))
-    }
+		type: Sequelize.TEXT,
+		get() {
+			return makeMatrix(this.getDataValue('data'))
+		},
+		set(matrix) {
+			this.setDataValue(
+				'data',
+				matrix
+					.reduce((acc, curr) => {
+						acc = acc.concat(curr)
+						return acc
+					}, [])
+					.join('')
+			)
+		}
 	},
-  STA: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  END: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  BMB: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  XTM: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  FRZ: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  TEL: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  PRT: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  time: {
-    type: Sequelize.INTEGER
-  }
+	STA: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	END: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	BMB: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	XTM: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	FRZ: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	TEL: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	PRT: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER)
+	},
+	time: {
+		type: Sequelize.INTEGER
+	}
 })
 
 // setter takes a matrix and sets 'data' value on maze instance as a binary string
 // getter returns the binary string in its matrix format
+
+Maze.beforeCreate((maze, options) => {
+	maze.name = generateName()
+})
 
 module.exports = Maze
