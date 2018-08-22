1d181bf8d49fb2741b4596747384239fe0a520a5
Ali Ebner <ebner.ali@gmail.com>
2018-08-21 21:24:59 -0400

    Feature(user profile) - a user can see their challenges and mazes

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,16 +1,72 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { loadChallenges, loadMazes } from '../store/user'
+import { saveMaze } from '../store/maze'
+import history from '../history'
+import axios from 'axios'
 
 class MyAccount extends Component {
-	// componentDidMount() {
-	// 	this.props.loadFriends(this.props.user.id)
-	// }
+	constructor(){
+		super()
+		this.handlePlay = this.handlePlay.bind(this)
+	}
+	async componentDidMount() {
+		await this.props.loadChallenges(this.props.user.id)
+		await this.props.loadMazes(this.props.user.id)
+	}
+
+	async handlePlay(evt) {
+		evt.preventDefault()
+		console.log(evt.target)
+		const maze = await axios.get(`api/mazes/${evt.target.value}`)
+		this.props.saveMaze(maze.data)
+		history.push('/pixi')
+	}
+
 	render() {
-		const { user } = this.props
+		console.log('props', this.props)
+		const { user, challenges, mazes } = this.props
+		if(!this.props.challenges || !this.props.mazes) {
+			return <h1>Loading...</h1>
+		}
 		return (
-			<div className="main">
-				<div className="circle" />
+			<div id="profile" className="main">
 				<h4>Welcome, {user.userName}</h4>
+			<div>
+				<div className="profile-list" id="challenges">
+				<h3>Challenges</h3>
+				{
+					challenges.length ? challenges.map(challenge => (
+						<div className="list-group list-group-flush" key={challenge.id}>
+						  <div className="list-group-item list-group-item-action flex-column align-items-start">
+						    <div className="d-flex w-100 justify-content-between">
+						      <h5 className="mb-1">{challenge.maze.name}</h5>
+						      <small>New</small>
+						    </div>
+						    <button type = "button" className="btn btn-primary" value={challenge.maze.id} onClick={this.handlePlay}>Play</button>
+						  	</div>
+						</div>
+					)) : <h5>No Challenges Yet!</h5>
+				}
+				</div>
+				<div className="profile-list" id="userMazes">
+					<h3>My Mazes</h3>
+					{
+						mazes.length ? mazes.map(maze => (
+							<div className="list-group list-group-flush" key={maze.id}>
+							  <div className="list-group-item list-group-item-action flex-column align-items-start">
+							    <div className="d-flex w-100 justify-content-between">
+							      <h5 className="mb-1">{maze.name}</h5>
+							      <button type="button" className="btn btn-primary" value={maze.id} onClick={this.handlePlay}>Play</button>
+							      {/*<button type="button" value={maze.id} onClick={this.handlePlay}><wired-button>Play</wired-button></button>*/}
+							    </div>
+							    
+							  </div>
+							</div>
+						)) : <h5>No Mazes Yet!</h5>
+					}
+				</div>
+			</div>
 			</div>
 		)
 	}
@@ -18,11 +74,21 @@ class MyAccount extends Component {
 
 const mapState = state => {
 	return {
-		user: state.user
+		user: state.user,
+		challenges: state.user.challenges,
+		mazes: state.user.mazes
+	}
+}
+
+const mapDispatch = dispatch => {
+	return {
+		loadChallenges: (id) => dispatch(loadChallenges(id)),
+		loadMazes: (id) => dispatch(loadMazes(id)),
+		saveMaze: (data) => dispatch(saveMaze(data))
 	}
 }
 
 export default connect(
 	mapState,
-	null
+	mapDispatch
 )(MyAccount)
