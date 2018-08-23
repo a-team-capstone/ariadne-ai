92b340c373d834efec3d17d1d34f4819129bc416
Isabel Hirama <isabel.hirama@gmail.com>
2018-08-23 15:03:03 -0400

    fix (PixiGame) removed event listeners after use

--- client/src/utilities/keyboardTracker.js
+++ client/src/utilities/keyboardTracker.js
@@ -1,3 +1,7 @@
+window.downHandlers = []
+window.upHandlers = []
+
+
 const keyboardTracker = function(keyCode) {
   let key = {};
   key.code = keyCode;
@@ -13,24 +17,31 @@ const keyboardTracker = function(keyCode) {
       key.isUp = false;
     }
     event.preventDefault();
-  };
+	};
+
 
   //The `upHandler`
   key.upHandler = event => {
-    if (event.keyCode === key.code) {
-      if (key.isDown && key.release) key.release();
+		if (event.keyCode === key.code) {
+			if (key.isDown && key.release) key.release();
       key.isDown = false;
       key.isUp = true;
     }
     event.preventDefault();
-  };
+	};
+
+	let down = key.downHandler.bind(key)
+	let up = key.upHandler.bind(key)
+
+	window.downHandlers.push(down)
+	window.upHandlers.push(up)
 
   //Attach event listeners
   window.addEventListener(
-    "keydown", key.downHandler.bind(key), false
+    "keydown", down, false
   );
   window.addEventListener(
-    "keyup", key.upHandler.bind(key), false
+    "keyup", up, false
   );
   return key;
 }
